6540ab648a6b393bd1e2dcee942efed905524ada
Vivek Kumar Pandey <148194511+Vivek-pandey101@users.noreply.github.com>
2024-04-06 15:45:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+white.addEventListener('click', () => {
+    const white = document.getElementById('white')
+    document.body.style.background = "white"
+})
+yellow.addEventListener('click', () => {
+    const white = document.getElementById('yellow')
+    document.body.style.backgroundColor = "yellow";
+})
+red.addEventListener('click', () => {
+    const white = document.getElementById('red')
+    document.body.style.backgroundColor = "red";
+})
+green.addEventListener('click', () => {
+    const white = document.getElementById('green')
+    document.body.style.backgroundColor = "green";
+})
